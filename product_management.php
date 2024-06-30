65ad61aa7e067cf7343c0d228eb10b9cf757b94e
SyedaUmmeNaima <atikamustarin@gmail.com>
2024-06-30 15:08:46 +0600

    Add Task A

---
+++ product_management.php
@@ -0,0 +1,42 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Product_Management";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+
+// Read only the rickshaw_id and name that has speed greater than 10
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['task']) && $_GET['task'] == 'task1') {
+$sql = "SELECT registration_number, name FROM Rickshaw WHERE speed > 10";
+$result = $conn->query($sql);
+
+$rickshaws = [];
+if ($result->num_rows > 0) {
+while ($row = $result->fetch_assoc()) {
+$rickshaws[] = $row;
+}
+}
+echo json_encode($rickshaws);
+}
+
+// Read all the driver information who has joined after June 24, 2023
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['task']) && $_GET['task'] == 'task2') {
+$sql = "SELECT d.* FROM Driver d JOIN Rickshaw_Driver_Mapping rdm ON d.driving_license_number = rdm.driver_id WHERE rdm.date_of_joining > '2023-06-24'";
+$result = $conn->query($sql);
+
+$drivers = [];
+if ($result->num_rows > 0) {
+while ($row = $result->fetch_assoc()) {
+$drivers[] = $row;
+}
+}
+echo json_encode($drivers);
+}
+?>
